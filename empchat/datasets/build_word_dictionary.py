fe3f1b3e42844d540b2c6479073e36fb5b4c9a3d
hikaru-y <h.yamada.bg@gmail.com>
2020-05-15 14:01:50 +0900

    jsonとして取り出しできた

--- empchat/datasets/build_word_dictionary.py
+++ empchat/datasets/build_word_dictionary.py
@@ -1,22 +1,16 @@
 # coding: UTF-8
 import pdb
+import json
 from pprint import pprint
+
 
 def build_dict_from_reddit_raw_data():
     f = open('../../reddit-folder/raw_data', 'r')
     line = f.readline()
-    word_dictionary = [f.readline() for i,l in enumerate(line) if i < 4]
-
-    # n = 0
-    # while line:
-    #     print(line)
-    #     line = f.readline()
-    #     n += 1
-    #     if n > 3:
-    #         break
-
+    word_dictionary = [json.loads(f.readline()) for i,l in enumerate(line) if i < 4]
     f.close
     pprint(word_dictionary)
 
 if __name__ == '__main__':
     build_dict_from_reddit_raw_data()
+    # json_load()
