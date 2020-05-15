f035ff9e81a98df2cef0b148475a0df0a99d2359
hikaru-y <h.yamada.bg@gmail.com>
2020-05-15 21:18:07 +0900

    words dict done

--- empchat/datasets/build_word_dictionary.py
+++ empchat/datasets/build_word_dictionary.py
@@ -11,6 +11,13 @@ def build_dict_from_reddit_raw_data():
     f.close
     pprint(word_dictionary)
 
+    '''
+    words: dict with words as keys and word idxs as tokens
+    iwords: list of words in order (where the index of each word is given by words, above)
+    wordcounts: 1D Tensor indexed the same way as iwords, where each value is the frequency of that word in the corpus
+    '''
+    words = [w['body'] if w['body'] != '[deleted]' else None for w in word_dictionary]
+    # pdb.set_trace()
 if __name__ == '__main__':
     build_dict_from_reddit_raw_data()
     # json_load()
