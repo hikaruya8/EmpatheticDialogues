7c39a9b5d2a0c54159bc921930cc8dc466e0e75d
hikaru-y <h.yamada.bg@gmail.com>
2020-05-26 16:14:08 +0900

    batch_size changed

--- simpleBERT/dataloader_BERT_finetuning.py
+++ simpleBERT/dataloader_BERT_finetuning.py
@@ -1,17 +1,8 @@
-import json
-import string
-import re
-import os
-import urllib
-import zipfile
 import random
 import torchtext
-from torchtext.vocab import Vectors
 from transformers import BertTokenizer
 
-import ipdb
-
-def get_utterance_and_context_loader(max_length=256, batch_size=64):
+def get_utterance_and_context_loader(max_length=256, batch_size=32):
     max_length = max_length
     batch_size = batch_size
 
@@ -29,17 +20,19 @@ def get_utterance_and_context_loader(max_length=256, batch_size=64):
         init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
     CONTEXT_ALL = torchtext.data.Field(sequential=True, tokenize=tokenizer.encode, use_vocab=False, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
     LABEL = torchtext.data.Field(sequential=False, use_vocab=False, preprocessing=lambda l: 0 if l=='TRUE' else 1, is_target=True)
-   #  CONTEXT1 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT2 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT3 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT4 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT5 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT6 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT7 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT8 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT9 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   #  CONTEXT10 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
-   # CONTEXT11 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, lower=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index)
+
+    '''if use CONTEXT respectively'''
+   #  CONTEXT1 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT2 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT3 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT4 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT5 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT6 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT7 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT8 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT9 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   #  CONTEXT10 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
+   # CONTEXT11 =  torchtext.data.Field(sequential=True, tokenize=tokenizer, use_vocab=True, include_lengths=True, batch_first=True, fix_length=max_length, init_token=cls_index, eos_token=eos_index, pad_token=pad_index, unk_token=unk_index)
 
 
     ds = torchtext.data.TabularDataset(
@@ -62,33 +55,15 @@ def get_utterance_and_context_loader(max_length=256, batch_size=64):
     # print(f'train dataの数:{len(train_ds)}, validataion dataの数:{len(val_ds)}, test dataの数: {len(test_ds)}')
     # print(f'1つ目のデータ{vars(train_ds[1])}')
 
-
-    # english_fasttext_vectors = Vectors(name='data/wiki-news-300d-1M.vec')
-
-    # ベクトル化したバージョンのボキャブラリーを作成. (UTTERANCEとCONTEXTの2つのフィールドで同一のvocabを作成したため少し変則的)
-    # UTTERANCE.build_vocab(ds.utterance, ds.context_all, vectors=english_fasttext_vectors, min_freq=1)
-    # CONTEXT_ALL.vocab = UTTERANCE.vocab
-    # # 普通のbuild_vocab
-    # # UTTERANCE.build_vocab(ds, vectors=english_fasttext_vectors, min_freq=1)
-    # # CONTEXT_ALL.build_vocab(ds, vectors=english_fasttext_vectors, min_freq=1)
-    # SPEAKER.build_vocab(ds)
-
-    # ボキャブラリーのベクトルを確認
-    # print(UTTERANCE.vocab.vectors.shape)
-    # print(UTTERANCE.vocab.vectors)
-
-    # ボキャブラリーの単語の順番を確認
-    # print(CONTEXT_ALL.vocab.stoi)
-
     # make dataloader
-    train_dl = torchtext.data.Iterator(train_ds, batch_size=24, train=True)
-    val_dl = torchtext.data.Iterator(val_ds, batch_size=24, train=False, sort=False)
-    test_dl = torchtext.data.Iterator(test_ds, batch_size=24, train=False, sort=False)
+    train_dl = torchtext.data.Iterator(train_ds, batch_size=batch_size, train=True)
+    val_dl = torchtext.data.Iterator(val_ds, batch_size=batch_size, train=False, sort=False)
+    test_dl = torchtext.data.Iterator(test_ds, batch_size=batch_size, train=False, sort=False)
 
     # test   train_dataで確認
-    batch = next(iter(train_dl))
-    print(batch.utterance)
-    print(batch.label)
+    # batch = next(iter(train_dl))
+    # print(batch.utterance)
+    # print(batch.label)
 
 
 if __name__ == '__main__':
